fe68585612f9776428449ca7ad9b48941cba6066
Stefano Moro <morostefano96@gmail.com>
2023-09-21 17:33:04 +0200

    changed sch coordinates conversion, using linear regression

--- sar_processing/trajectories/computeENU2SCH.m
+++ sar_processing/trajectories/computeENU2SCH.m
@@ -1,25 +1,18 @@
-function [enu2sch, center_enu]= computeENU2SCH(pos_enu)   
+function [enu2sch, center_enu]= computeENU2SCH(pos_enu)
 
-A = [pos_enu(:,1) pos_enu(:,2)];
-origin = mean(A,1);
-A = A - origin;
-[~,~,V] = svd(A' * A);
-% Transforming from NED to SCH with SVD. 
-% The transformation is done with Vt (V hermitian), knowing that A = U * diag(S) * Vt
-% ned2sch = [
-%     B.Vt[1,1] B.Vt[1,2] 0;
-%     B.Vt[2,1] B.Vt[2,2] 0;
-%     0 0 -1
-% ]
-Vt = V';
-enu2sch = [
-    Vt(1,1) Vt(1,2) 0;
-    Vt(2,1) Vt(2,2) 0;
-    0 0 1;
-    ];
+% linear regression of path
 
-center_enu = [origin , 0];
+x = pos_enu(1:10:end,1);
+y = pos_enu(1:10:end,2);
+center_enu = [mean(x),mean(y),0];
+x = x - center_enu(1);
+y = y - center_enu(2);
+X = [ones(length(x),1) x];
 
+b = X\y;
+ang = atan(b(2));
 
-
+enu2sch = [cos(ang) -sin(ang) 0;  ...
+    sin(ang) cos(ang) 0; ...
+    0 0 1];
 end
